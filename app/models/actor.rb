331ce464c2610035322a0e418de213ae93b70dd4
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-10-24 15:33:05 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,7 +6,7 @@ class Actor < ActiveRecord::Base
     name = "#{self.first_name} #{self.last_name}"
   end
 
-  def list_roles 
+  def list_roles
     roles = []
     array_of_characters = self.characters.map {|character| character.name}
     array_of_shows = self.shows.map {|shows| shows.name}
